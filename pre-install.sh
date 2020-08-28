92db4569509cbaa8c42afe9e3f5287a85f3f4863
shivakumarbalraj <70362210+shivakumarbalraj@users.noreply.github.com>
2020-08-28 17:10:33 +1000

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -1,10 +1,5 @@
 #! /bin/bash
 
-##### Pre-installation Task
-service httpd status
-systemctl stop nginx
-rm /var/www/html/index.html
-docker start 07c8882b4352
-docker ps
-service mysqld start
-service httpd start
+df -h
+ls -l
+whoami
